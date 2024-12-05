0a43bdf22548c6ef621b68f3898c3462f774c901
Alexis Metton <alexis.metton@gmail.com>
2024-12-05 22:39:14 +0100

    feat: fin test reussi

--- src/cypress.config.js
+++ src/cypress.config.js
@@ -3,9 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3001",
-    // pageLoadTimeout: 180000, // Temps d'attente pour charger les pages
-    // responseTimeout: 120000, // Temps d'attente pour les réponses HTTP
-    // defaultCommandTimeout: 10000, // Temps d'attente par commande Cypress
+
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
