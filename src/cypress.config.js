377e2dbf39431ae00a76114752940d8e4ab46e13
Alexis Metton <alexis.metton@gmail.com>
2024-12-05 21:56:28 +0100

    test11 e2e workflow

--- src/cypress.config.js
+++ src/cypress.config.js
@@ -3,9 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3001",
-    // pageLoadTimeout: 180000, // Augmentez à 3 minutes si nécessaire
-    // responseTimeout: 120000, // Attendre jusqu'à 2 minutes pour les réponses HTTP
-    // retries: 2,
+    pageLoadTimeout: 180000, // Temps d'attente pour charger les pages
+    responseTimeout: 120000, // Temps d'attente pour les réponses HTTP
+    defaultCommandTimeout: 10000, // Temps d'attente par commande Cypress
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
